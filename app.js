11346e2f341119385975fc2d7e2bebdf8f7ef735
georgelau <laugeorge@gmail.com>
2020-02-28 11:29:25 -0500

    configuring and maniulating DOM access

--- app.js
+++ app.js
@@ -10,4 +10,18 @@ GAME RULES:
 */
 
 // Getting the roll function
+var scores, roundScore, activePlayer;
 
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.round(Math.random() * 6) + 1;
+console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
